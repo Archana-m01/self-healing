9e4cbd2f3a3454eb94de0949dcd57d3ae2c64da0
Archana <archi.m.82966@gmail.com>
2026-05-12 20:17:40 +0530

    fixed chrome issue and duplicate run page

--- app.py
+++ app.py
@@ -151,15 +151,18 @@ def run():
                 "text": "Launching Chrome browser..."
             })
 
-            # Chrome options for Render / Headless deployment
             chrome_options = Options()
-            chrome_options.add_argument("--headless")
+
+            chrome_options.binary_location = "/usr/bin/google-chrome"
+
+            chrome_options.add_argument("--headless=new")
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--disable-gpu")
             chrome_options.add_argument("--window-size=1920,1080")
             chrome_options.add_argument("--disable-extensions")
             chrome_options.add_argument("--disable-infobars")
+            chrome_options.add_argument("--remote-debugging-port=9222")
 
             driver = webdriver.Chrome(options=chrome_options)
 
